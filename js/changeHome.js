16f25c792af8720ed7202eff82d21dcb12283709
Alma-Sanchez <alma08sanchez@gmail.com>
2016-02-28 11:03:44 -0500

    added sort functionality

--- js/changeHome.js
+++ js/changeHome.js
@@ -3,11 +3,80 @@ function change(){
 	var selectedIndex = selectMenu.selectedIndex;
 	var selected = 	selectMenu.options[selectedIndex].text;
 	var changeThis = document.getElementById("changeThis");
+	var itemClass =  document.getElementsByClassName(selected);
+	var profiles = [];
 
-	if("Other" == selected){
-		changeThis.innerHTML = "hello world";
-	}else{
-		changeThis.innerHTML = "";
-	}
+		if("HackTCNJ" == selected){
+			$('#portfolio').each(function(){
+				var portfolio = '';
+				$(this).find('li').each(function(){
+					var current = $(this);
+					// (current.children().size() > 0){	
+						if(!current.hasClass(selected)){
+							this.style.display = "none";
+							console.log("has hackTCNJ class");
+						}else{
+							this.style.display = "inline-block";
+						}
+					// }
+				})
+			})
+
+		}else if("ProfHacks" == selected){
+			$('#portfolio').each(function(){
+				var portfolio = '';
+				$(this).find('li').each(function(){
+					var current = $(this);
+					// (current.children().size() > 0){	
+						if(!current.hasClass(selected)){
+							this.style.display = "none";
+						}else{
+							this.style.display = "inline-block";
+						}
+					// }
+				})
+			})
+		}else if("HackPrinceton" == selected){
+			$('#portfolio').each(function(){
+				var portfolio = '';
+				$(this).find('li').each(function(){
+					var current = $(this);
+					// (current.children().size() > 0){	
+						if(!current.hasClass(selected)){
+							this.style.display = "none";
+						}else{
+							this.style.display = "inline-block";
+						}
+					// }
+				})
+			})
+		}else if("HackBCA" == selected){
+			$('#portfolio').each(function(){
+				var portfolio = '';
+				$(this).find('li').each(function(){
+					var current = $(this);
+					// (current.children().size() > 0){	
+						if(!current.hasClass(selected)){
+							this.style.display = "none";
+						}else{
+							this.style.display = "inline-block";
+						}
+					// }
+				})
+			})
+		}else if("HackRU" == selected){
+			$('#portfolio').each(function(){
+				var portfolio = '';
+				$(this).find('li').each(function(){
+					var current = $(this);
+					// (current.children().size() > 0){	
+						if(!current.hasClass(selected)){
+							this.style.display = "none";
+						}else{
+							this.style.display = "inline-block";
+						}
+					// }
+				})
+			})
+		}
 }
-// dropdown filter
